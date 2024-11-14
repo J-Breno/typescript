0d064ac7e6c8bac73ec562141b42629585765180
J-Breno <joao.breno85@hotmail.com>
2024-11-14 04:54:01 -0300

    Aprendendo os fundamentos do typescript

---
+++ 02_fundamentos/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const x = 10;
+const y = 15.4324;
+const firstName = "João";
+let fullName;
+const lastName = "Breno";
+fullName = firstName + " " + lastName;
+let a = true;
+// annotation é quando definimos o tipo de um dado manualmente;
+// inference é quando o TS identifica e define o tipo de dados para nós
---
+++ 02_fundamentos/index.ts
@@ -0,0 +1,9 @@
+const x:number = 10;
+const y:number = 15.4324
+const firstName:string = "João";
+let fullName:string;
+const lastName:string = "Breno";
+fullName = firstName + " " + lastName;
+let a:boolean = true;
+// annotation é quando definimos o tipo de um dado manualmente;
+// inference é quando o TS identifica e define o tipo de dados para nós
